addb539921b97c3c4ce1ecf3a8d2ea96600580c1
Aaron Fryer <aaronfvaio@msn.com>
2021-09-28 12:29:29 +0100

    updated test

--- src/__tests__/gilded_rose_specs.js
+++ src/__tests__/gilded_rose_specs.js
@@ -2,10 +2,13 @@ import { Shop, Item } from '../gilded_rose';
 
 describe("Gilded Rose", function () {
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    it("Brie should age", function () {
+        const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("foo");
+        expect(items[0].name).toEqual("Aged Brie");
+        gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(1);
+        expect(items[0].quality).toEqual(1);
     });
 
 });
